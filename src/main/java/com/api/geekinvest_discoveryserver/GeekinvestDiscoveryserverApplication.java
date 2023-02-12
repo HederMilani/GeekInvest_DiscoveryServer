7a97ea099edbcbadad0c18103d8f8b7573062155
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2023-02-11 23:39:29 -0300

    ADD - Configurações Eureka Server

--- src/main/java/com/api/geekinvest_discoveryserver/GeekinvestDiscoveryserverApplication.java
+++ src/main/java/com/api/geekinvest_discoveryserver/GeekinvestDiscoveryserverApplication.java
@@ -2,8 +2,10 @@ package com.api.geekinvest_discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class GeekinvestDiscoveryserverApplication {
 
 	public static void main(String[] args) {
