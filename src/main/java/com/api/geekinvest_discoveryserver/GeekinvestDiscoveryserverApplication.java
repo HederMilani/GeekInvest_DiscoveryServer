6225943bab99dfac090b8f01288618decc2d64a0
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2023-02-11 23:26:45 -0300

    CREATE - Criar service LoadBalance

---
+++ src/main/java/com/api/geekinvest_discoveryserver/GeekinvestDiscoveryserverApplication.java
@@ -0,0 +1,13 @@
+package com.api.geekinvest_discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekinvestDiscoveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekinvestDiscoveryserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/geekinvest_discoveryserver/GeekinvestDiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.geekinvest_discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekinvestDiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
